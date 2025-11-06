ba0ae7465b78a8e018e69e1987d6b5fdf6928758
Grady <arrangedgodly@gmail.com>
2025-11-06 13:15:05 -0700

    fixing image links to move to cloudinary

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -109,7 +109,7 @@ const Car = ({ car, currentUser, onRatingUpdate, onCollectionUpdate }) => {
       >
         <figure>
           <img
-            src={car.image}
+            src={car.cloudinary.secure_url}
             alt={car.name}
             className="w-full h-30 bg-white object-cover"
           />
